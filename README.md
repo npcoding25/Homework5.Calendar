776e3c35f73161f0ed9837fa71823147c2d71ef8
Nathan Polomark <polomarkn@hotmail.com>
2020-10-13 20:53:12 -0700

    Condensed code, added comments, started readme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Homework5.Calendar
+# Day Planner
+
+## Objective
+
+* Create an hourly day planner where you can write and save tasks
+
+## Functionality
+
+When opening up the day planner you will be given 
--- script.js
+++ script.js
@@ -1,9 +1,15 @@
+// Variables for placing current day and time
 var currentDay = document.querySelector("#currentDay")
 var currentTime = document.querySelector("#currentTime")
-currentDay.textContent = moment().format('MMMM Do YYYY')
-currentTime.textContent = moment().format('LT')
-console.log(currentDay.textContent)
 
+// Getting date from moment.js
+currentDay.textContent = moment().format('MMMM Do YYYY')
+
+// Getting time from moment.js
+currentTime.textContent = moment().format('LT')
+
+// Loop through time ids and compare to the time given by moment.js
+// Using info have different colours for past, present, future
 function showColor() {
     for ( var i=9; i<19; i++) {
         var time = i
@@ -21,57 +27,20 @@ function showColor() {
 showColor();
 
 // Saving inputs to local storage
-$("#saveBtn1").on("click", function(){
-    localStorage.text1 = $("#text1").val()
-})
-
-
-$("#saveBtn2").on("click", function(){
-    localStorage.text2 = $("#text2").val()
-})
-
-$("#saveBtn3").on("click", function(){
-    localStorage.text3 = $("#text3").val()
-})
-
-$("#saveBtn4").on("click", function(){
-    localStorage.text4 = $("#text4").val()
-})
-
-$("#saveBtn5").on("click", function(){
-    localStorage.text5 = $("#text5").val()
-})
-
-$("#saveBtn6").on("click", function(){
-    localStorage.text6 = $("#text6").val()
-})
-
-$("#saveBtn7").on("click", function(){
-    localStorage.text7 = $("#text7").val()
-})
-
-$("#saveBtn8").on("click", function(){
-    localStorage.text8 = $("#text8").val()
-})
-
-$("#saveBtn9").on("click", function(){
-    localStorage.text9 = $("#text9").val()
-})
-
-$("#saveBtn10").on("click", function(){
-    localStorage.text10 = $("#text10").val()
-})
-                    
+$("#saveBtn1").on("click", () => localStorage.text1 = $("#text1").val())
+$("#saveBtn2").on("click", () => localStorage.text2 = $("#text2").val())
+$("#saveBtn3").on("click", () => localStorage.text3 = $("#text3").val())
+$("#saveBtn4").on("click", () => localStorage.text4 = $("#text4").val())
+$("#saveBtn5").on("click", () => localStorage.text5 = $("#text5").val())
+$("#saveBtn6").on("click", () => localStorage.text6 = $("#text6").val())
+$("#saveBtn7").on("click", () => localStorage.text7 = $("#text7").val())
+$("#saveBtn8").on("click", () => localStorage.text8 = $("#text8").val())
+$("#saveBtn9").on("click", () => localStorage.text9 = $("#text9").val())
+$("#saveBtn10").on("click", () => localStorage.text10 = $("#text10").val())
+  
+// Loop through local storage and show previously saved tasks
 function getSavedTasks() {
-    $("#text1").val(localStorage.getItem("text1"))
-    $("#text2").val(localStorage.getItem("text2"))
-    $("#text3").val(localStorage.getItem("text3"))
-    $("#text4").val(localStorage.getItem("text4"))
-    $("#text5").val(localStorage.getItem("text5"))
-    $("#text6").val(localStorage.getItem("text6"))
-    $("#text7").val(localStorage.getItem("text7"))
-    $("#text8").val(localStorage.getItem("text8"))
-    $("#text9").val(localStorage.getItem("text9"))
-    $("#text10").val(localStorage.getItem("text10"))
+    for (let i=1; i<11; i++)
+    $("#text"+ i).val(localStorage.getItem("text"+ i))
 }
 getSavedTasks()
